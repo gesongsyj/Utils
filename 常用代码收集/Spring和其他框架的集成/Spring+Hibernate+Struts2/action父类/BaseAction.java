3664a0ceec49f3e13fb9b257388651ca107a1885
syj <1971119509@qq.com>
2017-11-20 10:16:17 +0800

    Action父类中增加给客户端返回html或json数据的方法

--- 常用代码收集/Spring和其他框架的集成/Spring+Hibernate+Struts2/action父类/BaseAction.java
+++ 常用代码收集/Spring和其他框架的集成/Spring+Hibernate+Struts2/action父类/BaseAction.java
@@ -1,5 +1,8 @@
 package com.xmg.pps.web.action;
 
+import org.apache.struts2.ServletActionContext;
+
+import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.Preparable;
@@ -12,5 +15,23 @@ public class BaseAction extends ActionSupport implements Preparable{
 	//把数据存放到context区域
 	public void putContext(String name,Object value){
 		ActionContext.getContext().put(name, value);
+	}
+	
+	/**
+	 * 给客户端响应html格式数据
+	 * @throws IOException 
+	 */
+	protected void putResponseText(String data) throws Exception{
+		ServletActionContext.getResponse().setContentType("text/html;charset=utf-8");
+		ServletActionContext.getResponse().getWriter().print(data);
+	}
+	/**
+	 * 给客户端响应json格式数据
+	 * @throws IOException 
+	 */
+	protected void putResponseJson(Object obj) throws Exception{
+		String jsonString = JSON.toJSONString(obj);
+		ServletActionContext.getResponse().setContentType("text/html;charset=utf-8");
+		ServletActionContext.getResponse().getWriter().print(jsonString);
 	}
 }
