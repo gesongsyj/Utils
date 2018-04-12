bf88cfe57794ee08b763bbc24dcc11db7445c3b4
宋焰君 <1971119509@qq.com>
2018-04-12 16:10:06 +0800

    111

--- 常用代码收集/状态码的处理/BitStatesUtils.java
+++ 常用代码收集/状态码的处理/BitStatesUtils.java
@@ -7,14 +7,14 @@ package com.syj.java.base.utils;
  */
 public class BitStatesUtils {
 	public final static Long OP_BIND_PHONE = 1L; // 用户绑定手机状态码
-	public final static Long OP_BIND_EMAIL = 2L << 0; // 用户绑定邮箱状态码
+	public final static Long OP_BIND_EMAIL = 1L << 1; // 用户绑定邮箱状态码
 
-	public final static Long OP_BASE_INFO = 2L << 1; // 用户填写基本资料状态码
-	public final static Long OP_REAL_AUTH = 2L << 2; // 用户实名认证状态码
-	public final static Long OP_VEDIO_AUTH = 2L << 3; // 用户视频认证状态码
-	public final static Long OP_HAS_BIDREQUST_IN_PROCESS = 2L << 4;// 用户的当前有一个标正在审核流程中
-	public final static Long OP_BIND_BANKINFO = 2L << 5;// 用户绑定银行卡
-	public final static Long OP_HAS_MONEYWITHDRAW = 2L << 6;// 用户当前是否有提现申请
+	public final static Long OP_BASE_INFO = 1L << 2; // 用户填写基本资料状态码
+	public final static Long OP_REAL_AUTH = 1L << 3; // 用户实名认证状态码
+	public final static Long OP_VEDIO_AUTH = 1L << 4; // 用户视频认证状态码
+	public final static Long OP_HAS_BIDREQUST_IN_PROCESS = 1L << 5;// 用户的当前有一个标正在审核流程中
+	public final static Long OP_BIND_BANKINFO = 1L << 6;// 用户绑定银行卡
+	public final static Long OP_HAS_MONEYWITHDRAW = 1L << 7;// 用户当前是否有提现申请
 
 	/**
 	 * @param states
